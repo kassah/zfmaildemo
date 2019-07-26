d19a67566de0dcbc3aeca12b97e736b8cf1cd6f2
William Lightning <wlightning@fuelmedical.com>
2019-07-25 21:56:02 -0700

    Impliment example Zend Mail usage from docs

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -9,11 +9,28 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Mail\Transport\Smtp as SmtpTransport;
+use Zend\Mail\Transport\SmtpOptions;
+use Zend\Mail;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+        $mail = new Mail\Message();
+        $mail->setBody('This is the text of the email.');
+        $mail->setFrom('Freeaqingme@example.org', "Sender's name");
+        $mail->addTo('Matthew@example.com', 'Name of recipient');
+        $mail->setSubject('TestSubject');
+
+        $transport = new SmtpTransport();
+        $options   = new SmtpOptions([
+            'name' => 'zf',
+            'host' => 'mailhog',
+            'port' => 1025,
+        ]);
+        $transport->setOptions($options);
+        $transport->send($mail);
         return new ViewModel();
     }
 }
